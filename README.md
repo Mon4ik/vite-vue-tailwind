607e35a35a4c8600cc03e18feb4716b4fdcf8f07
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2023-12-15 12:31:40 +0300

    feat: first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vite + Vue + Tailwind
+```shell
+git clone https://github.com/Mon4ik/vite-vue-tailwind.git
+cd vite-vue-tailwind
+
+pnpm i # or `npm i`, `yarn`
+```
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="text-5xl text-center p-2">
+      <p><b>Vite</b> + <b>Vue</b> + <b>Tailwind</b> = <a href="https://github.com/Mon4ik/vite-vue-tailwind">Mon4ik/vite-vue-tailwind</a></p>
+  </main>
+</template>
+
+<style lang="scss" src="./assets/scss/main.scss" />
